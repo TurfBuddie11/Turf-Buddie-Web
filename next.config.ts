e70dc9adfb8db438454505f3ad29800d9e928259
Anurag Borkar <anuragborkar2005@gmail.com>
2025-11-20 10:20:37 +0000

    Fix Postal Code api issue

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
       `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
       `img-src 'self' data: blob: https://firebasestorage.googleapis.com https://*.razorpay.com`,
       `font-src 'self' https://fonts.gstatic.com`,
-      `connect-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://firebasestorage.googleapis.com https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.google-analytics.com`,
+      `connect-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://firebasestorage.googleapis.com https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.google-analytics.com https://api.postalpincode.in`,
       `frame-src https://*.razorpay.com`,
       `object-src 'none'`,
       `base-uri 'self'`,
